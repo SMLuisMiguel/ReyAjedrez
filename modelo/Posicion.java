e360b4b0e4944e0747fe0837c58d36757cedfc8d
Luis_Miguel <lumisamo_1983@hotmail.com>
2023-11-17 22:50:39 +0100

    He creado los metodos getter y setter y se han validado los datos de la clase Posicion.

--- modelo/Posicion.java
+++ modelo/Posicion.java
@@ -6,4 +6,44 @@ public class Posicion {
     private int fila;
 
     private char columna;
+
+
+    public int getFila()
+    {
+        return fila;
+    }
+
+    public void setFila(int fila)
+    {
+        if (fila >= 1 && fila <= 8)
+        {
+            this.fila = fila;
+        }
+        else
+        {
+            throw new IllegalArgumentException("La fila introducida no es correcta");
+        }
+
+    }
+
+    public char getColumna()
+    {
+        return columna;
+    }
+
+    public void setColumna(char columna)
+    {
+        if (columna >= 'a' && columna <= 'h')
+        {
+            this.columna = columna;
+        }
+        else
+        {
+            throw new IllegalArgumentException("La columna introducida no es correcta.");
+        }
+
+    }
 }
+
+
+

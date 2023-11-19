322630ba1e9325462ca56182d1ba4981df46c26c
Luis_Miguel <lumisamo_1983@hotmail.com>
2023-11-19 20:59:53 +0100

    Se crea los metodos setter y getter de la clase Rey.

--- modelo/Rey.java
+++ modelo/Rey.java
@@ -1,12 +1,43 @@
 package modelo;
 
 public class Rey {
-
- private Color color;
- private Posicion posicion;
- private int totalMovimientos;
-
+    private Color color;
+    private Posicion posicion;
+    private int totalMovimientos;
 
 
+    public Color getColor()
+    {
+        return color;
+    }
+
+    private void setColor(Color color)
+    {
+        if (color != null)
+        {
+            this.color = color;
+        }
+        else
+        {
+            throw new NullPointerException("El color introducido no es el correcto");
+        }
+    }
+
+    public Posicion getPosicion()
+    {
+        return posicion;
+    }
+
+    private void setPosicion(Posicion posicion)
+    {
+        if (posicion != null)
+        {
+            this.posicion = posicion;
+        }
+        else
+        {
+            throw new NullPointerException("La posicion introducida no es la correcta");
+        }
+    }
 }
 

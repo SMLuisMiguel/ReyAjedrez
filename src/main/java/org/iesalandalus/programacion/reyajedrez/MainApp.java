5c30f5108a1432d84d04b1cc344203b12f57b796
Luis_Miguel <lumisamo_1983@hotmail.com>
2023-11-20 22:03:08 +0100

    Se crea el atributo rey en la clase MainApp.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -1,10 +1,18 @@
 package org.iesalandalus.programacion.reyajedrez;
-
 import org.iesalandalus.programacion.reyajedrez.modelo.Color;
 import org.iesalandalus.programacion.reyajedrez.modelo.Direccion;
+import org.iesalandalus.programacion.reyajedrez.modelo.Rey;
 
 public class MainApp {
+
+    private Rey rey;
+
+
     public static void main(String[] args) {
+
+
+
+
 
         Consola.mostrarMenu();
 
